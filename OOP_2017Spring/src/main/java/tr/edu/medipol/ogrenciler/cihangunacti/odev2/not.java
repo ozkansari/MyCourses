0726ffc27124f7ea8cb06e217c8b6482ae438aaa
Sari, Ozkan <Ozkan.Sari@mastercard.com>
2017-03-14 07:59:40 +0200

    Cihan not duzeltme

--- OOP_2017Spring/src/main/java/tr/edu/medipol/ogrenciler/cihangunacti/odev2/not.java
+++ OOP_2017Spring/src/main/java/tr/edu/medipol/ogrenciler/cihangunacti/odev2/not.java
@@ -1,43 +1,34 @@
 package tr.edu.medipol.ogrenciler.cihangunacti.odev2;
 
-
 public class not {
 
 	public static void main(String[] args) {
-		int[] ogrenciNotlari = 
-		      { 65, 80, 80, 20, 65, 65, 15, 80, 70 };
+		int[] ogrenciNotlari = { 65, 80, 80, 20, 65, 65, 15, 80, 70 };
 		System.out.println("Notlar");
-		
-		
+
 		for (int i = 0; i < ogrenciNotlari.length; i++) {
 			int notlar = ogrenciNotlari[i];
 			System.out.println(notlar);
-			
+
 		}
-		 		
-					
-		    int[] notlarIstatistik = new int[101]; 
-		    for (int i = 0; i < ogrenciNotlari.length; i++) {
-				int notDegeri = ogrenciNotlari[i];
-				notlarIstatistik[notDegeri]++;
+
+		int[] notlarIstatistik = new int[101];
+		for (int i = 0; i < ogrenciNotlari.length; i++) {
+			int notDegeri = ogrenciNotlari[i];
+			notlarIstatistik[notDegeri]++;
+		}
+
+		for (int n = 0; n < notlarIstatistik.length; n++) {
+
+			int notAlanSayisi = notlarIstatistik[n];
+
+			if (notAlanSayisi > 0) {
+				System.out.println(n + " : " + notAlanSayisi);
+
 			}
-			
-		   
-		  
-		    for (int n = 0; n < notlarIstatistik.length; n++) {  
-		      
-		      int notAlanSayisi = notlarIstatistik[n]; 
-		      
-		      if (notAlanSayisi > 0) {
-		        System.out.println(n + " : " + notAlanSayisi);
-		     
-		      }
-		      
-		    }
-		    
-		  }
-		
+
+		}
 
 	}
 
-
+}
