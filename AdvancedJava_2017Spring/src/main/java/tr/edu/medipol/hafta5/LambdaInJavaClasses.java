593c76f80999479071ed42c35905115b329812e7
Sari, Ozkan <Ozkan.Sari@mastercard.com>
2017-03-23 22:16:58 +0200

    Quiz

---
+++ AdvancedJava_2017Spring/src/main/java/tr/edu/medipol/hafta5/LambdaInJavaClasses.java
@@ -0,0 +1,22 @@
+package tr.edu.medipol.hafta5;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LambdaInJavaClasses {
+
+	public static void main(String[] args) {
+		List<Integer> list = new ArrayList<>();
+
+		for (int i = 0; i < 10; ++i)
+			list.add(i + 1);
+
+		Collections.sort(list, (a, b) -> b - a);
+		System.out.println(list);
+		
+		Collections.sort(list, (s1, s2) -> s1.compareTo(s2));
+		System.out.println(list);
+		
+	}
+}
