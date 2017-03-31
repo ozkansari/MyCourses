5233d78756e3c97e10afbf778af461c0a30fa7a8
medipol <medipol@10.201.67.195>
2017-03-31 10:51:29 +0300

    Hafta 6 ileri java

--- OOP_2017Spring/src/main/java/tr/edu/medipol/hafta6/grub_a/SayiArama.java
+++ OOP_2017Spring/src/main/java/tr/edu/medipol/hafta6/grub_a/SayiArama.java
@@ -1,9 +1,9 @@
 package tr.edu.medipol.hafta6.grub_a;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SayiArama {
-	
 	// "sayiDizisi" icinde "arananSayi"yi bulun
 	public static int sayiArama(int[] sayiDizisi, int arananSayi) {
 		for (int i = 0; i < sayiDizisi.length; i++) {
@@ -13,7 +13,6 @@ public class SayiArama {
 		}
 		return -1;
 	}
-	
 	public static void main(String[] args) {
 		Scanner konsolOkuyucu = new Scanner(System.in);
 		// kullanicidan 10 tane sayi input alin
@@ -23,23 +22,26 @@ public class SayiArama {
 		for (int i = 0; i < 10; i++) {
 			dizi[i] = konsolOkuyucu.nextInt();
 		}
-		
 		// kullanicidan aranacak sayiyi input alin
 		System.out.println("Dizide aramak istediginiz sayiyi giriniz:");
 		int aranan = konsolOkuyucu.nextInt();
-		
 		// mainden sayiArama'yi cagir (int dizisi ve aranan sayi ile)
-		int bulunanIndis = sayiArama(dizi, aranan);
+		// int bulunanIndis = sayiArama(dizi, aranan);
+		int bulunanIndis = Arrays.binarySearch(dizi, aranan);
 		if (bulunanIndis == -1) {
 			System.out.println("Sayi bulunamadi");
 		} else {
 			System.out.println("Sayi bulundu. Indis: " + bulunanIndis);
 		}
-
-		int [] dizi2 = {21,1234,234234,343,43,3};
-		int bulunanIndis2 = sayiArama(dizi2, aranan);
-		
 		konsolOkuyucu.close();
 	}
-
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
