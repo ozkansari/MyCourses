9154144227e9a5ed77a37083714e2f2936bd3fd5
Sari, Ozkan <Ozkan.Sari@mastercard.com>
2017-04-17 01:44:03 +0300

    Odev 1 degerlendirme (tamamlanmadi)

---
+++ AdvancedJava_2017Spring/src/main/java/tr/edu/medipol/odev1/cenkhanakdemir/DoktoraOgrencisi.java
@@ -0,0 +1,19 @@
+package tr.edu.medipol.odev1.cenkhanakdemir;
+
+public class DoktoraOgrencisi extends Ogrenci {
+
+	public DoktoraOgrencisi(String adSoyad, String bolum) {
+		super(adSoyad, bolum);
+	}
+
+	@Override
+	public String ogrenciBilgileriAl() {
+		return ogrenciTipiniAl() + " " +  adSoyad;
+	}
+	
+	@Override
+	public String ogrenciTipiniAl() {
+		return "Doktora Ogrencisi";
+	}
+
+}
