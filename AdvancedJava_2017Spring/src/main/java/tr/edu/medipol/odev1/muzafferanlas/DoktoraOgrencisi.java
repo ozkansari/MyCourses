ce4bdd1427bfe0c497f6e0e390991fd73e787dff
ozkansari <ozkansari@gmail.com>
2017-04-18 04:03:55 +0300

    ileri java odev 1 notlari (tamamlanmadi)

---
+++ AdvancedJava_2017Spring/src/main/java/tr/edu/medipol/odev1/muzafferanlas/DoktoraOgrencisi.java
@@ -0,0 +1,20 @@
+package tr.edu.medipol.odev1.muzafferanlas;
+
+public class DoktoraOgrencisi extends Ogrenci{
+	
+	public DoktoraOgrencisi(String adSoyad, String bolum) {
+		super(adSoyad, bolum);
+	}
+
+	@Override
+	public String ogrenciBilgileriAl() {
+		return ogrenciTipiniAl() + " " +  adSoyad;
+	}
+	
+	@Override
+	public String ogrenciTipiniAl() {
+		return "Doktora Ogrencisi";
+	}
+	
+
+}
