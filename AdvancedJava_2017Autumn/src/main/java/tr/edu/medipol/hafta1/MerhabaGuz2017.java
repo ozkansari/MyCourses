5aae521a081206e6ade824bc38160d28661c6d87
Sari, Ozkan <Ozkan.Sari@mastercard.com>
2017-10-11 06:57:19 -0430

    Advanced java sayfa duzenleme

---
+++ AdvancedJava_2017Autumn/src/main/java/tr/edu/medipol/hafta1/MerhabaGuz2017.java
@@ -0,0 +1,9 @@
+package tr.edu.medipol.hafta1;
+
+public class MerhabaGuz2017 {
+
+    public static void main(String[] args) {
+        System.out.println("Merhaba 2017 Guz donemi...");
+    }
+
+}
