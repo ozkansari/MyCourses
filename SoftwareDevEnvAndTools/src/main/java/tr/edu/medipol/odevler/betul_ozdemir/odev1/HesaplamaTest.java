29a487bcefa367e62409c63a4365bc9f2ee710b2
e048482 <ozkan.sari@mastercard.com>
2018-04-08 16:23:36 +0300

    Yazilim araclari not guncelleme

--- SoftwareDevEnvAndTools/src/main/java/tr/edu/medipol/odevler/betul_ozdemir/odev1/HesaplamaTest.java
+++ SoftwareDevEnvAndTools/src/main/java/tr/edu/medipol/odevler/betul_ozdemir/odev1/HesaplamaTest.java
@@ -6,44 +6,44 @@ import org.junit.*;
 public class HesaplamaTest {
 	@Test
 	public void KüpAlma(){
-		Assert.assertEquals(1000,MatematikselÝslemler.KüpAlma(10));
+		Assert.assertEquals(1000,MatematikselIslemler.KüpAlma(10));
 	
 	}
 	public void Bölme() {
-		int sonuc=MatematikselÝslemler.Bölme(20, 5);
+		int sonuc=MatematikselIslemler.Bölme(20, 5);
 	    Assert.assertEquals(4,sonuc);
 	   
 	}
 	public void Toplama() {
-		int sonuc=MatematikselÝslemler.Toplama(10, 10);
+		int sonuc=MatematikselIslemler.Toplama(10, 10);
 		Assert.assertEquals(20,sonuc);
 	}
-	public void Çýkarma() {
-		int sonuc=MatematikselÝslemler.Çýkarma(10, 2);
+	public void ÇIkarma() {
+		int sonuc=MatematikselIslemler.cikarma(10, 2);
 		Assert.assertEquals(8,sonuc);
 	}
 	public void Çarpma() {
-		int sonuc=MatematikselÝslemler.Çarpma(5, 2);
+		int sonuc=MatematikselIslemler.carpma(5, 2);
 		Assert.assertEquals(10,sonuc);
 	}
 	public void KökAlma() {
-		double sonuc=MatematikselÝslemler.KökAlma(7);
+		double sonuc=MatematikselIslemler.KökAlma(7);
 		Assert.assertEquals(2,64,sonuc);
 	}
 	public void MutlakDeðer() {
-		int sonuc=MatematikselÝslemler.MutlakDeðer(7);
+		int sonuc=MatematikselIslemler.MutlakDeger(7);
 		Assert.assertEquals(7,sonuc);
 	}
 	public void MinSayi() {
-		int sonuc=MatematikselÝslemler.MinSayi(10, 5);
+		int sonuc=MatematikselIslemler.MinSayi(10, 5);
 		Assert.assertEquals(5,sonuc);
 	}
 	public void MaxSayi() {
-		int sonuc=MatematikselÝslemler.MaxSayi(10, 5);
+		int sonuc=MatematikselIslemler.MaxSayi(10, 5);
 		Assert.assertEquals(10,sonuc);
 	}
 	public void ModAlma() {
-		int sonuc=MatematikselÝslemler.ModAlma(10, 5);
+		int sonuc=MatematikselIslemler.ModAlma(10, 5);
 		Assert.assertEquals(0,sonuc);
 }
 }
