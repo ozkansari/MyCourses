35da7e7b230d133c91be2c280a80152141250d63
ozkansari <ozkansari@gmail.com>
2018-10-27 11:40:28 +0300

    ders 2

---
+++ AdvancedJava/tr/edu/medipol/ilerijava/hafta2/SocketIstemci1.java
@@ -0,0 +1,39 @@
+package tr.edu.medipol.ilerijava.hafta2;
+
+import java.io.*;
+import java.net.*;
+
+public class SocketIstemci1 {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		System.out.println("Istemci aciliyor.");
+
+		PrintWriter sunucuMesajGonderici = null;
+		try {
+			Socket sunucuBaglantisi = new Socket("10.200.144.11", 
+					SocketSunucu1.SERVER_PORT);
+
+			sunucuMesajGonderici = 
+					new PrintWriter(sunucuBaglantisi.getOutputStream());
+
+		} catch (UnknownHostException e) {
+			System.out.println("Sunucu IP hatali" + e.getMessage());
+		} catch (IOException e) {
+			System.out.println("Sunucuya baglanirken hata alindi:" + 
+					e.getMessage());
+		}
+		
+		int i = 1;
+		do  {
+			String mesaj = "Mesaj " + i;
+			System.out.println(mesaj + " godneriliyor.");
+			sunucuMesajGonderici.println(mesaj);
+			sunucuMesajGonderici.flush();
+			i++;
+			Thread.sleep(5000);
+		} while(true);
+
+	}
+
+}
---
+++ AdvancedJava/tr/edu/medipol/ilerijava/hafta2/SocketSunucu1.java
@@ -0,0 +1,41 @@
+package tr.edu.medipol.ilerijava.hafta2;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class SocketSunucu1 {
+
+	public static final int SERVER_PORT = 7777;
+	
+	public static void main(String[] args) {
+		
+		try {
+			ServerSocket sunucuSocketi = new ServerSocket(SERVER_PORT);
+			
+			System.out.println("Istemciler bekleniyor." + 
+					sunucuSocketi.getLocalPort());
+			
+			while(true) {
+			
+				// Istemciden baglanti gelene kadar bekle
+				Socket istemciBaglantisi = sunucuSocketi.accept();
+				
+				System.out.println("Istemci baglandi: " + 
+						istemciBaglantisi.getInetAddress());
+				
+				SocketIstemciBaglantisiThread t = 
+						new SocketIstemciBaglantisiThread(istemciBaglantisi);
+				t.start();
+			
+			}
+			
+			
+		} catch (IOException e) {
+			System.out.println(SERVER_PORT + 
+					" Ustunden sunucu socketi acarken hata olustu");
+			e.printStackTrace();
+		}
+		
+	}
+}
