0fb70a0dc9193a4b3c438960872bf212735cc557
bprlab <bprlab@10.201.64.224>
2019-04-27 10:20:31 +0300

    derby veritabani basit ornek

---
+++ VeritabaniProje/src/main/java/DerbyVeritabaniOrnek1.java
@@ -0,0 +1,46 @@
+
+import java.sql.*;
+import java.util.*;
+
+// import org.apache.derby.jdbc.EmbeddedDriver;
+
+public class DerbyVeritabaniOrnek1 {
+
+	public static void main(String[] args) throws Exception {
+		
+		Class.forName("org.apache.derby.jdbc.EmbeddedDriver").newInstance();
+		
+		String url = "jdbc:derby:./.veritabani1/;create=true;";
+		Connection veritabaniBaglantisi = DriverManager.getConnection(url);
+		
+		Statement veritabaniKomutu = veritabaniBaglantisi.createStatement();
+		
+		// CREATE TABLE - Tablo Olustur - Sadece ilk defa calistiginda basarili calisir
+		try  {
+			String createTableSql = "CREATE TABLE ogrenciler ( isim varchar(40), yas int)";
+			veritabaniKomutu.executeUpdate(createTableSql);
+		} catch(Exception e) {
+			System.out.println("Tablo zaten mevcut" + e.getMessage());
+		}
+		
+		// Konsoldan oku
+		Scanner consolOkuyucu = new Scanner(System.in);
+		System.out.println("Isim Gir ve Enter'a bas: ");
+		String isim = consolOkuyucu.nextLine();
+		// INSERT INTO : Veritabanina kaiyt ekle
+		veritabaniKomutu.executeUpdate(
+				"INSERT INTO ogrenciler VALUES ('" + isim + "', 21)"
+		);
+		
+		// SELECT : Veritabanini sorgula, sonuclari goster
+		ResultSet sonuclar = veritabaniKomutu.executeQuery(
+				"SELECT * FROM ogrenciler WHERE yas>=18"
+		);
+		while(sonuclar.next()) {
+			String sonucIsim = sonuclar.getString("isim");
+			int sonucYas = sonuclar.getInt(2);
+			System.out.println(sonucIsim + " " + sonucYas);
+		}
+	}
+
+}
