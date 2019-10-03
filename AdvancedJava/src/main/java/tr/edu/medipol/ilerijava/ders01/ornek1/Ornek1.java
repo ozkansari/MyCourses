9b2dfc65577450039900134e1226a41a47c5664e
bprlab <bprlab@10.201.64.221>
2019-10-03 19:33:13 +0300

    Ders 1 ornekler

--- AdvancedJava/src/main/java/tr/edu/medipol/ilerijava/ders01/ornek1/Ornek1.java
+++ AdvancedJava/src/main/java/tr/edu/medipol/ilerijava/ders01/ornek1/Ornek1.java
@@ -9,6 +9,7 @@ public class Ornek1 {
 	 */
 	public static void main(String [] parametreler ) {
 		
+		/*--
 		SayiIslemleri degisken1 = new SayiIslemleri(5,3);
 		int sonuc1A = degisken1.topla();
 		System.out.println(sonuc1A); 
@@ -18,16 +19,19 @@ public class Ornek1 {
 		SayiIslemleri degisken2 = new SayiIslemleri(50,6);
 		int sonuc2A = degisken2.topla();
 		System.out.println(sonuc2A); 
-		
+		*/
 		
 		int sayi1 = Integer.valueOf( parametreler[0] );
 		int sayi2 = Integer.valueOf( parametreler[1] );
 		
-		SayiIslemleri degisken3 = new SayiIslemleri(sayi1, sayi2);
-		System.out.println( sayi1 + "+" + sayi2 + "=" + degisken3.topla() );
-		System.out.println("Yapilan islem sayisi: " + SayiIslemleri.islemSayisi);
 		
-		System.out.println(parametreler[0] + parametreler[1]);
+		SayiIslemleri degisken3 = new SayiIslemleri(sayi1, sayi2);
+		System.out.println( sayi1 + "+" + sayi2 + "=" 
+		   + degisken3.topla() );
+		System.out.println("Yapilan islem sayisi: " 
+		   + SayiIslemleri.islemSayisi);
+		
+		// System.out.println(parametreler[0] + parametreler[1]);
 	}
 	
 	
--- AdvancedJava/src/main/java/tr/edu/medipol/ilerijava/ders01/ornek1/SayiIslemleri.java
+++ AdvancedJava/src/main/java/tr/edu/medipol/ilerijava/ders01/ornek1/SayiIslemleri.java
@@ -11,19 +11,21 @@ public class SayiIslemleri {
 	/**
 	 *  Constructor
 	 */
-	SayiIslemleri(final int sayi1, final int sayi2) {
+	SayiIslemleri(final int sayi1, 
+			final int sayi2) {
 		this.sayi1 = sayi1;
 		this.sayi2 = sayi2;
 	}
 	
 	public int topla() {
 		// return sayi1 + sayi2;
-		return SayiIslemleri.topla(sayi1, sayi2);
+		return SayiIslemleri
+				.topla(sayi1, sayi2);
 	}
 	
-	public static int topla(int sayi1, int sayi2) {
-		islemSayisi++;
-		return sayi1 + sayi2;
+	public static int topla(int s1, int s2) {
+		SayiIslemleri.islemSayisi++;
+		return s1 + s2;
 	}
 	
 	
