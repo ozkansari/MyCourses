24633e8bb9ab35bca7289fd141fb24256116ddc5
ozkansari <ozkansari@gmail.com>
2020-01-09 16:28:01 +0300

    ornek 3

---
+++ WebOrnek1/src/main/java/spark/template/freemarker/ornek3/WebOrnek3.java
@@ -0,0 +1,43 @@
+package spark.template.freemarker.ornek3;
+
+import spark.*;
+import spark.template.freemarker.*;
+import static spark.Spark.*;
+
+public class WebOrnek3 {
+	public static void main(String[] args) {
+		
+		// http://localhost:4567/medipol/ornek/sayfa1
+		Route sayfa1 = new Route("/medipol/ornek/sayfa1") {
+			@Override
+			public Object handle(Request request, Response response) {
+				System.out.println("Sayfa 1 cagrildi. Request: " + 
+						request);
+				return "sayfa 1";
+			}
+		};
+		get(sayfa1);
+		
+		// http://localhost:4567/medipol/ornek/sayfa2
+		FreeMarkerRoute sayfa2 = new FreeMarkerRoute("/medipol/ornek/sayfa2") {
+			@Override
+			public Object handle(Request request, Response response) {
+				System.out.println("Sayfa 2 cagrildi. Request: " + 
+						request.body());
+				return new ModelAndView( null , "/ornek3/sayfa2.html" );
+			}
+		};
+		get(sayfa2);
+		
+		// http://localhost:4567/medipol/ornek/sayfa3
+		FreeMarkerRoute sayfa3 = new FreeMarkerRoute("/medipol/ornek/sayfa3") {
+			@Override
+			public Object handle(Request request, Response response) {
+				System.out.println("Anasayfa cagrildi. Request: " + 
+						request.body());
+				return new ModelAndView( null , "/ornek3/sayfa3.html" );
+			}
+		};
+		get(sayfa3);
+	}
+}
